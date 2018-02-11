ff267acbbc07fdede40497097cfcb53c55a9b740
Zak Nesler <zaknesler@gmail.com>
2018-02-10 20:08:07 -0500

    Add metric calculation

--- BMI-Calculator/app/src/main/java/com/zaknesler/bmicalculator/MainActivity.java
+++ BMI-Calculator/app/src/main/java/com/zaknesler/bmicalculator/MainActivity.java
@@ -3,8 +3,11 @@ package com.zaknesler.bmicalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,65 +15,106 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView heightValue, weightValue, button, resultValue, resultDescription;
+    private Spinner spinner;
+    private double finalBMI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText heightValue = findViewById(R.id.heightValue);
-        final EditText weightValue = findViewById(R.id.weightValue);
+        spinner = findViewById(R.id.spinner);
+        heightValue = findViewById(R.id.heightValue);
+        weightValue = findViewById(R.id.weightValue);
+        button = findViewById(R.id.calculateButton);
+        resultValue = findViewById(R.id.resultValue);
+        resultDescription = findViewById(R.id.resultDescription);
 
-        final TextView resultValue = findViewById(R.id.resultValue);
-        final TextView resultDescription = findViewById(R.id.resultDescription);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.units, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                heightValue.setText("");
+                weightValue.setText("");
+            }
 
-        final Button button = findViewById(R.id.calculateButton);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view)
             {
-                if (weightValue.getText().toString().equalsIgnoreCase("") || heightValue.getText().toString().equalsIgnoreCase("")) {
-                    Toast.makeText(getBaseContext(), "Values must not be empty.", Toast.LENGTH_LONG).show();
+            if (weightValue.getText().toString().equalsIgnoreCase("") || heightValue.getText().toString().equalsIgnoreCase("")) {
+                Toast.makeText(getBaseContext(), "Values must not be empty.", Toast.LENGTH_LONG).show();
 
-                    return;
-                }
+                return;
+            }
 
-                double weight = stringToDouble(weightValue);
-                double height = stringToDouble(heightValue);
-
-                double result = calculateBMI(weight, height);
-
-                resultValue.setText(formatResult(result));
-
-                if (result < 18.5) {
-                    resultDescription.setText(getString(R.string.result_description_underweight));
-                }
-
-                if ((result >= 18.5) &&  (result <= 24.9)) {
-                    resultDescription.setText(getString(R.string.result_description_normal));
-                }
-
-                if (result > 24.9) {
-                    resultDescription.setText(getString(R.string.result_description_overweight));
-                }
+            calculate();
             }
         });
     }
 
-    protected String formatResult(double value)
+    protected void calculate()
     {
-        DecimalFormat formatter = new DecimalFormat("0.00");
+        String[] items = getResources().getStringArray(R.array.units);
 
-        return getString(R.string.result_value, formatter.format(value));
+        String selected = items[spinner.getSelectedItemPosition()];
+
+        if (selected.equalsIgnoreCase(items[0])) {
+            finalBMI = calculateMetricBMI(stringToDouble(weightValue), stringToDouble(heightValue));
+        }
+
+        if (selected.equalsIgnoreCase(items[1])) {
+            finalBMI = calculateImperialBMI(stringToDouble(weightValue), stringToDouble(heightValue));
+        }
+
+        displayResults(finalBMI);
+    }
+
+    protected void displayResults(double value)
+    {
+        resultValue.setText(getString(R.string.result_value, Double.toString(value)));
+
+        if (value < 18.5) {
+            resultDescription.setText(getString(R.string.result_description_underweight));
+        }
+
+        if ((value >= 18.5) && (value <= 24.9)) {
+            resultDescription.setText(getString(R.string.result_description_normal));
+        }
+
+        if ((value >= 25.0) && (value <= 29.9)) {
+            resultDescription.setText(getString(R.string.result_description_overweight));
+        }
+
+        if (value > 30) {
+            resultDescription.setText(getString(R.string.result_description_obese));
+        }
     }
 
     protected double stringToDouble(TextView value)
     {
-        return Double.valueOf(value.getText().toString());
+        return round(Double.valueOf(value.getText().toString()));
     }
 
-    protected double calculateBMI(double weight, double height)
+    protected double calculateMetricBMI(double weight, double height)
     {
-        return weight / (height * height);
+        return round(weight / Math.pow(height, 2));
+    }
+
+    protected double calculateImperialBMI(double weight, double height)
+    {
+        return round((weight * 703) / Math.pow(height, 2));
+    }
+
+    protected double round(double value)
+    {
+        return Math.round(value * 10.0) / 10.0;
     }
 }
