fa4cee129727b5ff94eb52df97fb7631d88d4692
Zak Nesler <zaknesler@gmail.com>
2018-02-13 10:41:35 -0500

    Begin designing UI

--- WorldPopulation/app/src/main/java/com/zaknesler/worldpopulation/MainActivity.java
+++ WorldPopulation/app/src/main/java/com/zaknesler/worldpopulation/MainActivity.java
@@ -3,8 +3,16 @@ package com.zaknesler.worldpopulation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+import java.math.BigDecimal;
+
 public class MainActivity extends AppCompatActivity
 {
+    private static double growthRate = 0.0109;
+    private static BigDecimal currentPopulation = new BigDecimal("7601714600");
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
