cdb503954eb0b966279059ab914632d7b1a73ad2
Zak Nesler <zaknesler@gmail.com>
2018-02-14 11:56:48 -0500

    Finish working calculator

--- WorldPopulation/app/src/main/java/com/zaknesler/worldpopulation/MainActivity.java
+++ WorldPopulation/app/src/main/java/com/zaknesler/worldpopulation/MainActivity.java
@@ -2,14 +2,19 @@ package com.zaknesler.worldpopulation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
-import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity
 {
     private static double growthRate = 0.0109;
-    private static BigDecimal currentPopulation = new BigDecimal("7601714600");
+    private static long currentPopulation = 7601714600L;
+
+    private static DecimalFormat formatter = new DecimalFormat("#,###,###,###");
+
+    private TextView currentPopulationValue, growthRateValue, yearInput, resultTop, resultBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,8 +22,58 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView currentPopulationValue = findViewById(R.id.value_current_population);
+        currentPopulationValue = findViewById(R.id.value_current_population);
+        growthRateValue = findViewById(R.id.value_growth_rate);
+        yearInput = findViewById(R.id.input_year);
+        resultTop = findViewById(R.id.result_top);
+        resultBottom = findViewById(R.id.result_bottom);
 
-        currentPopulationValue.setText(currentPopulation.toPlainString());
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                resultTop.setText(getResources().getString(
+                        R.string.result_top,
+                        getYear()
+                ));
+
+                resultBottom.setText(getResources().getString(
+                        R.string.result_bottom,
+                        formatter.format(calculateWorldPopulation(getYearDifference(getYear())))
+                ));
+            }
+        });
+
+        displayInformation();
+    }
+
+    private long getYear()
+    {
+        try {
+            return Long.valueOf(yearInput.getText().toString());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    private long calculateWorldPopulation(long difference)
+    {
+        return (long) (currentPopulation * Math.exp(growthRate * difference));
+    }
+
+    private long getYearDifference(long check)
+    {
+        return check - 2018;
+    }
+
+    private void displayInformation()
+    {
+        currentPopulationValue.setText(formatter.format(currentPopulation));
+        growthRateValue.setText(getResources().getString(
+                R.string.value_growth_rate,
+                Double.toString(growthRate * 100)
+        ));
     }
 }
