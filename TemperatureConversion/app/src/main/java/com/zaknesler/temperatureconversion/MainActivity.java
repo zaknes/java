14f48df4550177beee1a5dbe09807ed6fd777380
Zak Nesler <zaknesler@gmail.com>
2018-02-15 10:33:53 -0500

    Tidy formatting

--- TemperatureConversion/app/src/main/java/com/zaknesler/temperatureconversion/MainActivity.java
+++ TemperatureConversion/app/src/main/java/com/zaknesler/temperatureconversion/MainActivity.java
@@ -1,12 +1,15 @@
 package com.zaknesler.temperatureconversion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Conversion conversion;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
