403a91d8f8dc127a769f661e0bc8573894c9c906
Zak Nesler <zaknesler@gmail.com>
2018-04-30 21:09:02 -0400

    Basic scanning functionality

--- BarcodeScanner/app/src/main/java/com/zaknesler/barcodescanner/MainActivity.java
+++ BarcodeScanner/app/src/main/java/com/zaknesler/barcodescanner/MainActivity.java
@@ -1,7 +1,17 @@
 package com.zaknesler.barcodescanner;
 
+import android.content.Intent;
+import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import devliving.online.mvbarcodereader.BarcodeCaptureFragment;
+import devliving.online.mvbarcodereader.MVBarcodeScanner;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +20,42 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        initializeScanner();
+    }
+
+    private void initializeScanner()
+    {
+        BarcodeCaptureFragment fragment = BarcodeCaptureFragment.instantiate(MVBarcodeScanner.ScanningMode.SINGLE_MANUAL, Barcode.ALL_FORMATS);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
+
+        fragment.setListener(new BarcodeCaptureFragment.BarcodeScanningListener()
+        {
+
+            @Override
+            public void onBarcodeScanned(Barcode barcode)
+            {
+                Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+
+                intent.putExtra("barcode", barcode.displayValue);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onBarcodesScanned(List<Barcode> barcodes)
+            {
+                //
+            }
+
+            @Override
+            public void onBarcodeScanningFailed(String reason)
+            {
+                Log.d("Barcode Scanner Fail", reason);
+            }
+        });
     }
 }
